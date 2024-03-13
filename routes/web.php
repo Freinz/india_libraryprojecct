791961d789595db0a875e07277dd8d9cdd6e09cc
Freinz <toriqthalib94@gmail.com>
2024-03-13 14:42:14 +0800

    meintegrasikan template kedalam laravel website, dan mengubah page awal laravel

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,9 +15,7 @@ use App\Http\Controllers\AdminController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class,'index']);
 
 Route::middleware([
     'auth:sanctum',
